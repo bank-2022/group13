947f215752cae04d7a5da1af955af7186b9d3539
TeemuMalo <101553328+TeemuMalo@users.noreply.github.com>
2022-05-04 22:41:03 +0300

    Add files via upload

--- api/app.js
+++ api/app.js
@@ -3,10 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const helmet = require('helmet');
+const cors = require('cors');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var asiakasRouter = require('./routes/asiakas');
+var tiliRouter = require('./routes/tili');
+var tilitapahtumaRouter = require('./routes/tilitapahtuma');
+var nostoRouter = require ('./routes/nosto');
+var korttiRouter = require ('./routes/kortti');
+var saldoRouter = require ('./routes/saldo');
+var kirjautuminenRouter = require ('./routes/kirjautuminen');
 
 var app = express();
+
+app.use(helmet());
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +27,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/asiakas', asiakasRouter);
+app.use('/tili', tiliRouter);
+app.use('/tilitapahtuma', tilitapahtumaRouter);
+app.use('/nosto', nostoRouter);
+app.use('/kortti', korttiRouter);
+app.use('/Saldo', saldoRouter);
+app.use('/kirjautuminen', kirjautuminenRouter);
+
+
 
 module.exports = app;
